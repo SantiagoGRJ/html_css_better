1dcdc632fbd01efc8fd8568b3b098fddced5df6e
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2026-03-06 15:28:14 -0600

    feat: add js

---
+++ js/js-changing-html/app.js
@@ -0,0 +1,10 @@
+const element = document.getElementById("id01")
+element.innerHTML="New Heading"
+document.getElementById("p1").innerHTML="New text!"
+
+
+document.getElementById("myImage").src="favicon.svg"
+
+document.getElementById("demo").innerHTML="Date : "+ Date()
+
+
